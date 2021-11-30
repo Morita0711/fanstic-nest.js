8c20f5c145f33360eaa1d73c8fe77bb85c5acae1
tinkerbell07 <tinkerbelldeveloper0711@gmail.com>
2021-12-01 03:29:49 +0900

    fix: error

--- src/cats.module.ts
+++ src/cats.module.ts
@@ -4,6 +4,7 @@ import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 import { Cat, CatSchema } from './cats.schema';
 
+//define the module 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   controllers: [CatsController],
